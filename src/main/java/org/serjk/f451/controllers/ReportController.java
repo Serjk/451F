c9efe4c063c55244267da7e07fde02985eec3f8b
Bedash Dmitry <tonerhps@google.com>
2014-06-22 21:18:44 +0400

    [Add] Reports functional

--- src/main/java/org/serjk/f451/controllers/ReportController.java
+++ src/main/java/org/serjk/f451/controllers/ReportController.java
@@ -279,4 +279,48 @@ public class ReportController {
         return  reportService.getByStepSimpleReportList(stepId);
     }
 
+
+
+    @RequestMapping("/admin/workflow")
+    public String getWorkFlowAdmin(Model model) {
+        model.addAttribute("transition", new Transition());
+        model.addAttribute("listTransition", workFlowService.listTransition());
+        model.addAttribute("permission", userLoginService.getRoles());
+        model.addAttribute("step", new Step());
+        model.addAttribute("listStep", workFlowService.listStep());
+        return "workflow";
+    }
+
+    @RequestMapping(value = "/admin/workflow/transition/new", method = RequestMethod.POST)
+    public String setNewTransition(@RequestParam(value ="name") String name,
+                                   @RequestParam(value ="stepIn") long stepIn,
+                                   @RequestParam(value ="stepOut") long stepOut,
+                                   @RequestParam(value ="permission") String permission) {
+
+        Transition transition = new Transition();
+        transition.setName(name);
+        transition.setStepIn(stepIn);
+        transition.setStepOut(stepOut);
+        transition.setPermission(permission);
+        workFlowService.addTransition(transition);
+        return "redirect:/admin/workflow";
+    }
+
+
+    @RequestMapping(value = "/admin/workflow/step/new", method = RequestMethod.POST)
+    public String setNewStep(@RequestParam("stepName") String stepName,
+                             @RequestParam("stepSummary") String stepSummary) {
+        Step step  = new Step();
+        step.setStepName(stepName);
+        step.setStepSummary(stepSummary);
+        workFlowService.addStep(step);
+        return "redirect:/admin/workflow";
+    }
+
+    @RequestMapping("/user/workflow/step/{stepId}")
+    public @ResponseBody
+    Step getStepInJSON(@PathVariable("stepId") long stepId) {
+        return workFlowService.getStep(stepId);
+    }
+
 }
